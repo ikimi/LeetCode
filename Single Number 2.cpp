be788fa8efb906f962dd1b9914deacdbcd275132
ikimi <jiajun.li13@gmail.com>
2014-02-26 18:25:06 +0800

    AC Single Number 2 by bit operation

--- Single Number 2.cpp
+++ Single Number 2.cpp
@@ -30,13 +30,68 @@ public:
     }
 };
 
+// 位运算
+class Solution2
+{
+public:
+    int singleNumber(int A[], int n)
+    {
+        int i, j, len, id, res, count;
+
+        len = sizeof(int) * 8, res = 0, id = 1;
+        for (i = 0; i < len; i++)
+        {
+            for (j = count = 0; j < n; j++)
+            {
+                if (A[j] & id)
+                    count++;
+            }
+            if (count % 3)
+                res ^= id;
+            id <<= 1;
+        }
+
+        return res;
+    }
+};
+
+// 位运算 n个数字除了出现一次的数字外，其他的数字在每一位上'1'出现的次数都为3的倍数
+// 将这些1置0 则最后剩余的就是出现一次的数字在这一bit上的值
+// 思路同方法二 只不过方法三的代码更简洁
+// 其实就是利用二进制来模拟三进制
+class Solution3
+{
+public:
+    int singleNumber(int A[], int n)
+    {
+        int i, ones, twos, threes;
+
+        ones = twos = threes = 0;
+        for (i = 0; i < n; i++)
+        {
+            twos |= ones & A[i];
+            ones ^= A[i];
+            threes = ~(ones & twos);
+            ones &= threes;
+            twos &= threes;
+        }
+
+        return ones;
+    }
+};
+
 int main()
 {
     // test example
     Solution1 s1;
+    Solution2 s2;
+    Solution3 s3;
 
-    int A[] = {1, 2, 2, 4, 2, 1, 1};
-    cout << s1.singleNumber(A, 7) << endl;
+    int A[] = {1, 2, 2, 4, 2, 1, 1, 4, 5, 4};
+    cout << s1.singleNumber(A, 10) << endl;
+    cout << s2.singleNumber(A, 10) << endl;
+    cout << s3.singleNumber(A, 10) << endl;
 
     return 0;
 }
+
