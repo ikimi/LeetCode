5bb9acc02063935b0544e580be0cfd2b5d8fd2db
ikimi <jiajun.li13@gmail.com>
2014-04-04 16:34:21 +0800

    AC Zigzag Conversion

---
+++ Zigzag Conversion.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+// 132ms
+class Solution1
+{
+public:
+    string convert(string s, int nRows)
+    {
+        if (nRows <= 1)
+            return s;
+
+        string res;
+        int i, j, temp = 2 * nRows - 2, gap, len;
+        len = s.length();
+
+        for (i = 0, gap = temp; i < len && i < nRows; i++)
+        {
+            gap = temp - 2 * i;
+            for (j = i; j < len;)
+            {
+                res += s[j];
+                if (!gap)
+                {
+                    gap = temp - gap;
+                    j += gap;
+                    continue;
+                }
+                j += gap;
+                gap = temp - gap;
+            }
+        }
+
+        return res;
+    }
+};
+
+int main()
+{
+    // test example
+    Solution1 s1;
+
+    string res;
+    string str1 = "PAYPALISHIRING";
+    string str2 = "PAYPALISHIRING";
+
+    res = s1.convert(str1, 3);
+    //res = s1.convert(str2, 4);
+    cout << res << endl;
+
+    return 0;
+}
